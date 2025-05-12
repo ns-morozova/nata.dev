41901863efc5d1e331894b9d316ddc7234b0070e
ns-morozova <128983577+ns-morozova@users.noreply.github.com>
2025-05-12 20:55:48 +0300

    fixed navbar

--- src/marketing/Navbar.tsx
+++ src/marketing/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
 import { Dialog, DialogPanel } from '@headlessui/react';
+import { RiHome9Fill } from "react-icons/ri";
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
 const Navbar: React.FC = () => {
@@ -25,19 +26,14 @@ const Navbar: React.FC = () => {
                     ></div>
 
                     <div className="relative flex items-center justify-end px-3 py-3 sm:px-4 sm:py-3 md:px-5 md:py-3.5 lg:justify-center lg:py-4.5">
-                        <div className="absolute left-1.5 flex lg:flex-1">
+                        <div className="absolute left-3 flex sm:left-4 md:left-8 lg:flex-1">
                             <ScrollLink
                                 to="home"
                                 smooth={true}
                                 duration={1000}
                                 className="-m-1.5 p-1.5 cursor-pointer"
                             >
-                                <div className="w-10 md:w-11 lg:w-13 h-auto ">
-                                    <img
-                                        src="/nata.dev/favicon-developer.svg"
-                                        className="w-full h-full object-cover"
-                                    />
-                                </div>
+                                <RiHome9Fill className="size-6" />
                             </ScrollLink>
                         </div>
 
@@ -69,27 +65,27 @@ const Navbar: React.FC = () => {
                 </nav>
             </div>
 
-            <Dialog open={mobileMenuOpen} onClose={() => setMobileMenuOpen(false)} className="lg:hidden">
-                <div className="fixed inset-0 z-10 bg-black/50" />
-                <DialogPanel className="fixed inset-y-0 right-0 w-full overflow-y-auto bg-[#141414] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10 z-50">
+            <Dialog open={mobileMenuOpen} onClose={() => setMobileMenuOpen(false)} className="lg:hidden h-screen overflow-hidden">
+                <div className="fixed inset-0 z-10 bg-black/50 overflow-hidden" />
+                <DialogPanel className="fixed inset-y-0 right-0 w-full overflow-y-auto bg-[#141414] px-6 pt-3 sm:max-w-sm sm:ring-1 sm:ring-white/10 z-50 overflow-hidden">
                     <div className="flex items-center justify-end">
                         <button
                             type="button"
                             onClick={() => setMobileMenuOpen(false)}
-                            className="-m-2.5 rounded-md p-2.5"
+                            className="-mr-3 rounded-md"
                         >
                             <span className="sr-only">Close menu</span>
                             <XMarkIcon aria-hidden="true" className="size-6" />
                         </button>
                     </div>
-                    <div className="mt-6 flow-root">
+                    <div className="relative mt-6 flow-root">
                         <div className="-my-6 divide-y divide-gray-500/25">
-                            <div className="font-medium text-base flex flex-col gap-6 py-6">
+                            <div className="font-medium text-base flex flex-col items-center justify-center gap-6 pt-36 py-auto">
                                 {menuItems.map((item) => (
                                     <Link
                                         key={item.id}
                                         to={item.to}
-                                        className="relative -mx-3 w-max group hover:text-teal-200 transition-colors duration-300 px-1 cursor-pointer"
+                                        className="relative w-max group hover:text-teal-200 transition-colors duration-300 px-1 cursor-pointer"
                                         onClick={() => setMobileMenuOpen(false)}
                                     >
                                         {item.label}
@@ -98,14 +94,6 @@ const Navbar: React.FC = () => {
                                 ))}
                             </div>
                         </div>
-                    </div>
-
-                    <div className="w-32 h-auto mx-auto mt-44">
-                        <img
-                            alt="logo"
-                            src="/nata.dev/favicon-developer.svg"
-                            className="w-full h-full object-cover"
-                        />
                     </div>
                 </DialogPanel>
             </Dialog>
